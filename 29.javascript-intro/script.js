43070ebcccb921d775c5238b58cc4618a018313c
purveshjambhulkar <purveshjambhulkar16@gmail.com>
2024-01-16 19:59:50 +0530

    new commit

---
+++ 29.javascript-intro/script.js
@@ -0,0 +1,16 @@
+console.log("Hello world")
+
+let a = "hello";
+let b = 500;
+
+const obj = {
+    name: "Purvesh" ,
+    roll: "400"
+}
+
+console.log(a)
+console.log(b)
+console.log(obj)
+console.log(obj.name)
+console.log(obj.roll)
+console.log("JAVASCRIPT begins")
