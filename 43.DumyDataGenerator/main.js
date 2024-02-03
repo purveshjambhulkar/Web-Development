4ebcc0c6efe7d86cc442525fdb9b57c1e89c3790
purveshjambhulkar <purveshjambhulkar16@gmail.com>
2024-02-03 22:08:49 +0530

    new commit

---
+++ 43.DumyDataGenerator/main.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index', { foo: 'FOO' });
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
