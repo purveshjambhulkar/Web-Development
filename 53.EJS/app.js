723a1009333a1c77d5bee80f16ac67f6dd96a026
purveshjambhulkar <purveshjambhulkar16@gmail.com>
2024-02-12 23:00:38 +0530

    new commit

--- 53.EJS/app.js
+++ 53.EJS/app.js
@@ -2,14 +2,19 @@ const express = require("express");
 
 const app = express();
 
+const path = require("path");
+
 let port = 8080;
 
 app.set("view engine" , "ejs");
 
-app.get("/", (req, res) => {
-    res.render("home.ejs");
-});
+app.set("views" , path.join(__dirname ,"/views" ));
+
 app.listen(port, () => {
     console.log(`listening at port ${port}`);
+    
+});
 
+app.get("/", (req, res) => {
+    res.render("home.ejs");
 });
