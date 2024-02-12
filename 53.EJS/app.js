9fb420f274c8e7e61cf92f42cfb19e5107b3b81d
purveshjambhulkar <purveshjambhulkar16@gmail.com>
2024-02-13 00:00:54 +0530

    new commit

--- 53.EJS/app.js
+++ 53.EJS/app.js
@@ -6,15 +6,21 @@ const path = require("path");
 
 let port = 8080;
 
-app.set("view engine" , "ejs");
+app.set("view engine", "ejs");
 
-app.set("views" , path.join(__dirname ,"/views" ));
+app.set("views", path.join(__dirname, "/views"));
 
 app.listen(port, () => {
     console.log(`listening at port ${port}`);
-    
+
 });
 
 app.get("/", (req, res) => {
     res.render("home.ejs");
 });
+
+
+app.get("/dice", (req, res) => {
+    let dice_value = Math.floor(Math.random() * 6) + 1;
+    res.render("diceroll.ejs", { num: dice_value });
+}); 
---
+++ 54.instagram_ejs/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+let port = 8080;
+app.set("view engine" , "ejs");
+app.set("views", path.join(__dirname , "/views"));
+
+app.listen(port ,()=>{
+    console.log(`the server is listening at ${port}`);  
+})
+
+app.get("/" , (req ,res)=>{
+    let dice = Math.floor(Math.random() * 6) + 1;
+    res.render("dice-roll.ejs" , { num : dice});
+})
+
+app.get("/insta/:username" , (req ,res)=>{
+    let follow = Math.floor(Math.random() * 1000000) + 1;
+    let {username} = req.params;
+    res.render("instagram.ejs" , {name : username , Followers : follow });
+})
