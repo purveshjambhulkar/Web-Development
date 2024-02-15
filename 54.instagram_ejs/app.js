9317189f7da506052299112c85287b8eb21a9a80
purveshjambhulkar <purveshjambhulkar16@gmail.com>
2024-02-15 20:06:26 +0530

    new commit

--- 54.instagram_ejs/app.js
+++ 54.instagram_ejs/app.js
@@ -5,6 +5,8 @@ let port = 8080;
 app.set("view engine" , "ejs");
 app.set("views", path.join(__dirname , "/views"));
 
+app.use(express.static("public"));
+
 app.listen(port ,()=>{
     console.log(`the server is listening at ${port}`);  
 })
