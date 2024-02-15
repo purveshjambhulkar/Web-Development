b4a14c85e4c965659b6ef65844d7e36031d4a864
purveshjambhulkar <purveshjambhulkar16@gmail.com>
2024-02-15 22:37:34 +0530

    new commit

--- 55.GET&POST-REQ/BACKEND/app.js
+++ 55.GET&POST-REQ/BACKEND/app.js
@@ -2,20 +2,27 @@ const express = require("express");
 const app = express();
 let port = 8080;
 
+// handling post request
+app.use(express.urlencoded({ extended: true }));
+
+
+
 app.listen(port, () => {
     console.log(`the server is listening at post : ${port}`);
 
 })
 
-app.get("/register" , (req , res)=>{
-    let {user , email} = req.query;
+app.get("/register", (req, res) => {
+    let { user, email } = req.query;
     // res.send("THIS IS GET RESPONSE")
     res.send(`Welcome ${user} !`)
     console.log(`this is get request`);
-    
+
 })
-app.post("/register" , (req , res)=>{
+app.post("/register", (req, res) => {
+    let { user, email } = req.body;
+    // res.send("THIS IS POST RESPONSE")
+    res.send(`the username is ${user} and email id is ${email}`)
     console.log(`this is post request`);
-    res.send("THIS IS POST RESPONSE")
-    
 })
+
