98646674e4d9d92f2d81ac6299e5c50b89161efb
purveshjambhulkar <purveshjambhulkar16@gmail.com>
2024-04-10 20:47:46 +0530

    Add express-session, update package-lock.json and app.js
    * Add express-session as a dependency
    * Update package-lock.json with the new version of express-session
    * Import express-session in app.js
    * Add session middleware in app.js
    * Add a route to count the number of sessions in app.js
    * Add dependencies of express-session in package-lock.json (cookie, cookie-signature, debug, depd, on-headers, parseurl, safe-buffer, uid-safe)
    * Add dependencies of cookie and uid-safe in package-lock.json (random-bytes)

--- 63.COOKES/app.js
+++ 63.COOKES/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
 
 
 
@@ -8,36 +10,57 @@ app.use(cookieParser("secret code"));
 
 
 //ROUTE TO SET COOKIES
-app.get("/setcookies" , (req , res)=>{
-    res.cookie("name" , "John");
-    res.cookie("age" , "20");
+app.get("/setcookies", (req, res) => {
+    res.cookie("name", "John");
+    res.cookie("age", "20");
 
     res.send("cookies are set");
 })
 
 //ROUTES TO ACCESS THE COOKIES
-app.get("/getcookies" , (req , res)=>{
+app.get("/getcookies", (req, res) => {
     console.dir(req.cookies);
     res.send("cookies accessed");
 })
 
-app.get("/greet" , (req , res)=>{
-    const {name} = req.cookies;
+app.get("/greet", (req, res) => {
+    const { name } = req.cookies;
     res.send(`welcome ${name}`);
 })
 
 
 
 //ROUTE FOR SIGNED COOKIES
-app.get("/setsignedcookies" , (req ,res)=>{
-    res.cookie("color"  , "pink" , {signed : true});
+app.get("/setsignedcookies", (req, res) => {
+    res.cookie("color", "pink", { signed: true });
     res.send("signed cookies are set");
 })
 
 //accessing signed cookies
-app.get("/getsignedcookies" , (req ,res)=>{
+app.get("/getsignedcookies", (req, res) => {
     console.dir(req.signedCookies);
     res.send("signed cookies printed");
+})
+
+//=========================================
+
+//dealing with express session
+app.use(session({
+    secret: "this is a secret",
+    resave: false,
+    saveUninitialized: true,
+}))
+
+//route to count the number of sessions
+app.get("/count", (req, res) => {
+    if (req.session.count) {
+        req.session.count++;
+    }
+    else {
+        req.session.count = 1;
+    }
+
+    res.send(`you visitied ${req.session.count} times`);
 })
 
 
